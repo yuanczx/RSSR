3ccc6fe52fd009e6cc5a2af6e3ae3950beef56d0
yuanczx <yuanczx@foxmail.com>
2026-03-16 20:20:40 +0800

    feature: automatic language detection refactor: rename locale files to use region-specific codes (en_US, zh_CN)

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,18 +1,27 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
+import LanguageDetector from "i18next-browser-languagedetector"
 
-import en from "./locales/en.json"
-import zh from "./locales/zh.json"
+import en_US from "./locales/en_US.json"
+import zh_CN from "./locales/zh_CN.json"
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: { translation: en },
-      zh: { translation: zh }
+    detection:{
+      order: ["querystring", "cookie", "localStorage", "navigator", "htmlTag"],
+      lookupQuerystring: "lng",
+      lookupCookie: "i18next",
+      lookupLocalStorage: "i18nextLng",
+      caches: ["localStorage", "cookie"],
+      cookieMinutes: 10,
     },
-    lng: "zh",
-    fallbackLng: "en",
+    resources: {
+      en_US: { translation: en_US },
+      zh_CN: { translation: zh_CN }
+    },
+    fallbackLng: "en_US",
     interpolation: {
       escapeValue: false
     }
